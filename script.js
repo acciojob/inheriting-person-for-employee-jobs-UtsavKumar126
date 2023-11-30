b8189b69db24226c7ba74f8af89399c667bf61c2
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-11-30 23:13:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
+// Person constructor function
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
-// Method added to Person prototype
-Person.prototype.greet = function () {
+// Adding greet method to the Person prototype
+Person.prototype.greet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 
 // Employee constructor function inheriting from Person
 function Employee(name, age, jobTitle) {
-  Person.call(this, name, age); // inherit name and age from Person
+  Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// Inherit Person prototype methods
+// Inheriting the Person prototype into the Employee prototype
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// Method added to Employee prototype
-Employee.prototype.jobGreet = function () {
+// Adding jobGreet method to the Employee prototype
+Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
-
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
