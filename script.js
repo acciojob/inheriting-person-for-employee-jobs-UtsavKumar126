3ce90691e1e9d086ad8297c8db5f572cdeafe7a8
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-11-30 23:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-// Person constructor function
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+class Person {
+	constructor(name,age){
+		this._name = name;
+		this._age = age;
+	}
+	greet(){
+		console.log("Hello, my name is "+this._name+" and I am "+this._age+" years old.")
+	}
 }
 
-// Adding greet method to the Person prototype
-Person.prototype.greet = function() {
-  console.log("Hello, my name is Bob, I am 30 years old, and my job title is Manager.");
-};
-
-// Employee constructor function inheriting from Person
-function Employee(name, age, jobTitle) {
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
+class Employee extends Person {
+    constructor(name, age, jobTitle){
+		super(name,age);
+		this._jobTitle = jobTitle;
+	}
+	jobGreet(){
+		console.log("Hello, my name is "+this._name+", I am "+this._age+" years old, and my job title is "+this._jobTitle+".");
+	}
+	
 }
-
-// Inheriting the Person prototype into the Employee prototype
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
-
-// Adding jobGreet method to the Employee prototype
-Employee.prototype.jobGreet = function() {
-  console.log("Hello, my name is Bob, I am 30 years old, and my job title is Manager.");
-};
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
